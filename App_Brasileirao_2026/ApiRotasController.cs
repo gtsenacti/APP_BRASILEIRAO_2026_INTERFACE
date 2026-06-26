8e29b6c7cd29dbb2dc0d9d9a6ab81b37192a9c08
softtware0104 <gt.senacti@gmail.com>
2026-06-25 21:44:56 -0300

    BRASILEIRAO2026: INTERFACE | Elaboração da Controller.

--- App_Brasileirao_2026/ApiRotasController.cs
+++ App_Brasileirao_2026/ApiRotasController.cs
@@ -11,6 +11,10 @@ namespace App_Brasileirao_2026
         private static readonly string baseUrl = "http://127.0.0.1:5000";
 
         public static string ConsultarSerieA => $"{baseUrl}/SerieA";
+        public static string ConsultarSerieB => $"{baseUrl}/SerieB";
+        public static string ConsultarSerieC => $"{baseUrl}/SerieC";
+        public static string ConsultarSerieD => $"{baseUrl}/SerieD";
+        public static string ConsultarSerieE => $"{baseUrl}/SerieE";
         // Adicione mais endpoints aqui, se quiser:
     }
 }
